72bdf1b333997d6436fddfaf490d65ddc0933a24
Braden Virdell <bradenvirdell@gmail.com>
2017-10-16 01:28:20 +0000

    resouces added in routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   resources :business
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'home#index'
-  root to: 'yelp_controller#index'
+  get '/business/', to: 'yelp_model#index'
+  
+  
 end
+ 
