dfe03f16e9141473c1866df2299291b42fce0d44
Braden Virdell <bradenvirdell@gmail.com>
2017-10-16 02:01:44 +0000

    tests updated

--- features/step_definitions/yelp_find_restaurants.rb
+++ features/step_definitions/yelp_find_restaurants.rb
@@ -1,13 +1,23 @@
 When(/^I shearch for restaurants$/)do
-    
+    vist 
 end
+
 Then(/^I should see a list of all restaurants within ten miles of me$/)do
+    expect(page).to have_content("Buisness: True")
 end
-When(/^$/)do
+
+When(/^I shearch for reasturants and none are within ten miles$/)do
+    vist 
 end
-Then(/^$/)do
+
+Then(/^I should recive a map of my local area with only my location and an notifiation that there are no restaurants within ten miles of me$/)do
+    expect(page).to have_no_contet("Buisness: False")
 end
-When(/^$/)do
+
+When(/^I need information from the yelp API$/)do
+    vist
 end
-Then(/^$/)do
+
+Then(/^I should not be over the maximum daily calls$/)do
+    expect(page).to have_content("Yelp: True")
 end
