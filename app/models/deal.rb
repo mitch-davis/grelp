04d611c09008efb8772a22c6af9ab4c95865eac0
Mitch Davis <mdavis19@uccs.edu>
2017-10-21 18:20:47 +0000

    added tests for deals view; added more sample data for deals

--- app/models/deal.rb
+++ app/models/deal.rb
@@ -14,6 +14,6 @@ class Deal < ApplicationRecord
   end
   
   def self.near_by_deals
-      return [{:Restaurant => 'Panera Bread', :Value => '10% off', :Offer_Code => 'GF35-34T', :Restrictions => 'Can only use once/day', :Exp_Date => '12/01/17', :longitude => '-2.3524542', :latitude => '124.563632'}, {:Restaurant => 'Panera Bread', :Value => '10% off', :Offer_Code => 'GF35-34T', :Restrictions => 'Can only use once/day', :Exp_Date => '12/01/17', :longitude => '-2.3524542', :latitude => '124.563632'}]
+      return [{:Restaurant => 'Smash Burger', :Value => '20% Off Classic Cheeseburger', :Offer_Code => 'GG35-31N', :Restrictions => 'One Time Use', :Exp_Date => '12/05/17', :longitude => '-2.3524542', :latitude => '124.563632'}, {:Restaurant => 'Il Vicino', :Value => 'Free Beer With Pizza Purchase', :Offer_Code => 'WD25-11B', :Restrictions => 'Happy Hour Only', :Exp_Date => '11/15/17', :longitude => '-2.3524542', :latitude => '124.563632'}, {:Restaurant => 'Panera Bread', :Value => '10% Off Any Soup', :Offer_Code => 'GF35-34T', :Restrictions => 'Once/Day', :Exp_Date => '12/01/17', :longitude => '-2.3524542', :latitude => '124.563632'}, {:Restaurant => 'Hacienda', :Value => 'Unlimited Chips and Salsa', :Offer_Code => 'GS56-12L', :Restrictions => 'None', :Exp_Date => '12/07/17', :longitude => '-2.3524542', :latitude => '124.563632'}]
   end
 end
