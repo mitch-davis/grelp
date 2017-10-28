193fe9d9060e823411bb391002f0b883a7f4d242
Mitch Davis <mdavis19@uccs.edu>
2017-10-28 17:53:45 +0000

    added some rspec tests for Deal model

--- spec/models/deal_spec.rb
+++ spec/models/deal_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Deal, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "near_by_deals" do
+     it "should be defined" do
+        expect {Deal.near_by_deals}.not_to raise_error
+     end
+      
+     it "returns an array" do
+        expect(Deal.near_by_deals).to be_a_kind_of Array
+     end
+     
+     it "encodes each deal as a hash" do
+        expect(Deal.near_by_deals.length > 0).to eq(true)
+        expect(Deal.near_by_deals[0]).to be_a_kind_of Hash
+     end
+     
+         
+  end
+    
+  #pending "add some examples to (or delete) #{__FILE__}"
 end
