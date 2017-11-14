3be4f17117b2a867ad9b7fc7aa7c3ee25cd11ea8
Mitch Davis <mdavis19@uccs.edu>
2017-11-14 23:17:03 +0000

    Yelp data for primary restaurant attributes

--- app/controllers/restaurant_controller.rb
+++ app/controllers/restaurant_controller.rb
@@ -1,4 +1,7 @@
 class RestaurantController < ApplicationController
     def index
+        @test = params["id"]
+        @restaurant = Yelp.getRestaurantByID params["id"]
+        @reviews = Yelp.getReviewByID params["id"]
     end
 end
