a9ed73d11a02763146ca18b19bacedac8fafb2ad
Mitch Davis <mdavis19@uccs.edu>
2017-11-14 22:03:54 +0000

    added /resaurant view

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'splash#index'
   get '/home/', to: 'home#index'
-  get '/business/', to: 'yelp#index'
+  get '/businesses/', to: 'yelp#index'
   get '/deals/', to: 'deals#index'
+  get '/restaurant/', to: 'restaurant#index'
   
   resources :deals
   post "/deals/new"    => "deals#new"
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171111220214) do
+ActiveRecord::Schema.define(version: 20171114212959) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,11 @@ ActiveRecord::Schema.define(version: 20171111220214) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "restaurants", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
