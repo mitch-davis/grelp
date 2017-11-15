f1ea4ef94f8403f3dea2e8fa589670c62f483c75
Mitch Davis <mdavis19@uccs.edu>
2017-11-15 00:51:39 +0000

    added coupon table and reviews on /restaurant

--- app/controllers/restaurant_controller.rb
+++ app/controllers/restaurant_controller.rb
@@ -3,5 +3,12 @@ class RestaurantController < ApplicationController
         @test = params["id"]
         @restaurant = Yelp.getRestaurantByID params["id"]
         @reviews = Yelp.getReviewByID params["id"]
+        
+        @reviews["reviews"].each do |rev|
+            rev["rating"] = Yelp.genRatingHTML(rev["rating"])
+        end
+        
+        @coupons = Deal.getDealsByName @restaurant["name"]
+        
     end
 end
