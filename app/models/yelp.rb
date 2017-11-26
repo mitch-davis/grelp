0d672f41959446fe6ffbea608e7170a60893fddf
John Schavey <jschavey@uccs.edu>
2017-11-26 18:17:58 +0000

    user friendly message for restaurants without price data

--- app/models/yelp.rb
+++ app/models/yelp.rb
@@ -2,7 +2,7 @@ class Yelp
     require "json"
     require "http"
     require "optparse"
-    
+
     @@CLIENT_ID = "KlCuB5YcLt3UlZuk9e8uRQ"
     @@CLIENT_SECRET = "6J6CPbOkVAabYGfqgDkK4gb4mYhngJaAS5pbzAxT3gHsn8et7xbODCxqFFE59xkM"
 
@@ -19,7 +19,7 @@ class Yelp
     @@DEFAULT_TERM = "dinner"
     @@DEFAULT_LOCATION = "San Francisco, CA"
     @@SEARCH_LIMIT = 50
-    
+
     def self.message
         "stub"
     end
@@ -27,56 +27,56 @@ class Yelp
     def self.near location
        return search("food",location[:latitude], location[:longitude],true)
     end
-    
+
     def self.attributes
         return ["Restaurants", "Ratings", "Price Range", "Hours of Operations", "Number of Coupons"]
     end
-    
+
     def self.yelp
         return @@CLIENT_ID
     end
-    
+
     def self.getRestaurantByID id
         url = "https://api.yelp.com/v3/businesses/" + id
         params = {
-            
+
         }
-        
+
         response = HTTP.auth(Yelp.bearer_token).get(url, params: params)
         response.parse
     end
-    
+
     def self.getReviewByID id
-        
+
         url = "https://api.yelp.com/v3/businesses/" + id + "/reviews"
         params = {
-            
+
         }
-        
+
         response = HTTP.auth(Yelp.bearer_token).get(url, params: params)
         response.parse
-        
+
     end
-    
+
     def self.bearer_token
         # Put the url together
         url = "#{@@API_HOST}#{@@TOKEN_PATH}"
-    
+
         raise "Please set your CLIENT_ID" if @@CLIENT_ID.nil?
         raise "Please set your CLIENT_SECRET" if @@CLIENT_SECRET.nil?
-        
+
         # Build our params hash
         params = {
             client_id: @@CLIENT_ID,
             client_secret: @@CLIENT_SECRET,
             grant_type: @@GRANT_TYPE
         }
-        
+
         response = HTTP.post(url, params: params)
         parsed = response.parse
         "#{parsed['token_type']} #{parsed['access_token']}"
     end
-    
+
     def self.search(term, latitude, longitude, open_now)
         url = "#{@@API_HOST}#{@@SEARCH_PATH}"
         params = {
@@ -87,9 +87,16 @@ class Yelp
             limit: @@SEARCH_LIMIT
         }
         response = HTTP.auth(Yelp.bearer_token).get(url, params: params)
-        response.parse
+        obj = response.parse
+        obj['businesses'].each do |business|
+            puts business
+            if !business.has_key? 'price' then
+                business[:price] = "No Price Data Yet"
+            end
+        end
+        obj
     end
-    
+
     #generates the HTML for star icons based off of rating numeric
     def self.genRatingHTML rating
         if rating < 1 or rating > 5
@@ -107,7 +114,7 @@ class Yelp
         end
         return @ratingHTML
     end
-    
+
     #def self.genRandomDeals restaurants
      #  @all_deals = ""
       # @deal_num = 0
@@ -118,5 +125,5 @@ class Yelp
        #end
        #return @all_deals
     #end
-    
+
 end
