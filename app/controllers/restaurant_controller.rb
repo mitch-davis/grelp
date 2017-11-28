59d1b2fb7a54f22bc5cd5526b1fc66a5e43f8dc8
Mitch Davis <mdavis19@uccs.edu>
2017-11-28 18:09:31 +0000

    added more error handling for nil arrays

--- app/controllers/restaurant_controller.rb
+++ app/controllers/restaurant_controller.rb
@@ -2,10 +2,12 @@ class RestaurantController < ApplicationController
     def index
         @test = params["id"]
         @restaurant = Yelp.getRestaurantByID params["id"]
-        @reviews = Yelp.getReviewByID params["id"]
+        @reviews = Yelp.getReviewByID "not"#params["id"]
         
-        @reviews["reviews"].each do |rev|
-            rev["rating"] = Yelp.genRatingHTML(rev["rating"])
+        if not @reviews["reviews"].eql? nil
+            @reviews["reviews"].each do |rev|
+                rev["rating"] = Yelp.genRatingHTML(rev["rating"])
+            end
         end
         
         @coupons = Deal.getDealsByID params["id"]
