43dcfc1be008b5f59bcc2a1d6ff44c4c0d1e39bf
John Schavey <jschavey@uccs.edu>
2017-11-29 03:12:50 +0000

    resolve deprecated syntax

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,18 @@
 # Commenting a Core Class to Appease Reek
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :require_location
 
   def initialize
     super
     @location = Hash.new
-    require_location
   end
 
   def require_location
-    if (!location = request.location)
-      redirect_to :back
+    if (!defined?(request.location)) then
+      flash[:notice] = "We were unable to obtain your location at this time.  Please try again later (refresh)."
     else
+      location = request.location
       if (!cookies.has_key?(:latitude) || !cookies.has_key?(:longitude)) then
         @location[:latitude] = location.latitude
         @location[:longitude] = location.longitude
