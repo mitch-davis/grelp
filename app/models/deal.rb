178bc33727da0fd69a6b545b2d3fa76343056ea0
Mitch Davis <mdavis19@uccs.edu>
2017-11-29 22:42:54 +0000

    added rspec for deal.rb

--- app/models/deal.rb
+++ app/models/deal.rb
@@ -1,28 +1,12 @@
 class Deal < ApplicationRecord
     
-    def self.get_cuisines
-        all_cuisines = []
-        self.all.each{|r| all_cuisines << r.cuisine if !(all_cuisines.include? r.cuisine)}
-        all_cuisines.sort!
-       
-    end
-    
-    def self.sort_by(column)
-        data = Deal.all
-        return data.order(column)
-    end
-    
     def self.randomCode
        return (0...8).map { (65 + rand(26)).chr }.join
     end
     
     def self.getDealsByID id
         @deals = Deal.where(:ID => id)
-        if @deals.eql? nil
-            return [{:Restaurant => "No Deals Found", :ID => '', :Deal_Title => '', :Offer_Code => '', :Exp_Date => '', :Description => '', :Restrictions => ''}]
-        else
-            return @deals
-        end
+        return @deals
     end
 
 end
